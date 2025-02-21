71da000e3f2f0ded4fead33cd0791e59655f0515
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-02-21 20:28:47 -0300

    fix: fixing output of query select

--- backend/internal/schemas/walletAcoes/queries/queries.go
+++ backend/internal/schemas/walletAcoes/queries/queries.go
@@ -2,6 +2,7 @@ package queries_wallet_acoes
 
 import (
 	repository "github.com/Julia-Marcal/fake-fintech/internal/database"
+	database_acoes "github.com/Julia-Marcal/fake-fintech/internal/schemas/acoes"
 	database "github.com/Julia-Marcal/fake-fintech/internal/schemas/walletAcoes"
 	_ "github.com/gin-gonic/gin"
 )
@@ -12,11 +13,19 @@ func Create(walletAcao *database.WalletAcoes) error {
 	return result.Error
 }
 
-func FindAcoesByWallet(walletId string) ([]database.WalletAcoes, error) {
+func FindAcoesByWallet(walletId string) ([]database_acoes.Acoes, error) {
 	db := repository.NewPostgres()
-	var walletAcoes []database.WalletAcoes
+	var acoes []database_acoes.Acoes
 
-	result := db.Where("wallet_id = ?", walletId).Find(&walletAcoes)
+	result := db.Table("wallet_acoes").
+		Select("acoes.*").
+		Joins("JOIN acoes ON wallet_acoes.acoes_id = acoes.id").
+		Where("wallet_acoes.wallet_id = ?", walletId).
+		Scan(&acoes)
 
-	return walletAcoes, result.Error
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return acoes, nil
 }
