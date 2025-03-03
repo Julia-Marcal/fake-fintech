108edb38643eaf56196ee9deadcafdca5684c3e9
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-03-02 21:12:33 -0300

    feat: starting to make form validation for register

--- frontend/src/app/features/register/register.component.ts
+++ frontend/src/app/features/register/register.component.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IconDirective } from '@coreui/icons-angular';
 import { ContainerComponent, RowComponent, ColComponent, TextColorDirective, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, FormControlDirective, ButtonDirective } from '@coreui/angular';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.scss'],
-    imports: [ContainerComponent, RowComponent, ColComponent, TextColorDirective, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, IconDirective, FormControlDirective, ButtonDirective]
+    imports: [
+      CommonModule, ReactiveFormsModule, ContainerComponent, RowComponent, 
+      ColComponent, TextColorDirective, CardComponent, CardBodyComponent, 
+      FormDirective, InputGroupComponent, InputGroupTextDirective, IconDirective, 
+      FormControlDirective, ButtonDirective
+    ]
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+  
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      age: ['', [Validators.required, Validators.min(18)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, { validators: this.passwordMatchValidator });
+  }
 
+  passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordsMismatch: true };
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      const formData = this.registerForm.value;
+      console.log('Form Data:', formData);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
