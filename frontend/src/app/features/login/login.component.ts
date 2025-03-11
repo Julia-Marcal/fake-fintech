1a88db4b310361eab1e976bd74718e2af7070728
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-03-10 21:15:18 -0300

    feat: adicionando toast no login

--- frontend/src/app/features/login/login.component.ts
+++ frontend/src/app/features/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgStyle } from '@angular/common';
+import { ToastService } from '../../../shared/services/toast/toast.service';
 import { IconDirective } from '@coreui/icons-angular';
 import {
   ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective,
@@ -11,30 +12,36 @@ import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms'
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    standalone: true,
-    imports: [
-      ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective,
-      CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective,
-      IconDirective, FormControlDirective, ButtonDirective, NgStyle, FormsModule
-    ],
-    
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  standalone: true,
+  imports: [
+    ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective,
+    CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective,
+    IconDirective, FormControlDirective, ButtonDirective, NgStyle, FormsModule
+  ],
+
 })
 export class LoginComponent {
   isLoading = false;
   email: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private toastService: ToastService) { }
 
   onSubmit() {
     if(this.isLoading) return
     this.isLoading = true
-
     this.authService.login(this.email, this.password).subscribe({
       next: (response) => {
+        this.toastService.showToast({
+          title: 'Sucesso',
+          message: 'Você conseguiu logar!',
+          duration: 3000, 
+          position: 'top-end'
+        });
+
         this.router.navigate(['/home']).then(() => {
           this.isLoading = false; 
         }).catch(() => {
