d06c1a26f3e6f3bc76864f72b0f347792dbbace2
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-06-21 16:38:25 -0300

    feat: adding wallets controller and service

--- backend/app/Http/Resources/WalletsResource.php
+++ backend/app/Http/Resources/WalletsResource.php
@@ -4,16 +4,15 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class WalletResource extends JsonResource
+class WalletsResource extends JsonResource
 {
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
             'balance' => $this->balance,
-            'currency' => $this->currencies,
+            'currency' => $this->currency,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
