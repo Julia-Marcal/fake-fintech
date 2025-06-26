88d8abbfd1d5b6c024eb61edee2e2e63f2c50fa0
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-06-25 22:11:30 -0300

    treating user without token

--- frontend/src/app/features/user/user.component.ts
+++ frontend/src/app/features/user/user.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, RowComponent } from '@coreui/angular';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { UserService, User } from './user.service';
-
+import { Router } from '@angular/router';
 import { AuthService } from '../../../shared/services/auth/auth.service';
+import { ToastService } from '../../../shared/services/toast/toast.service';
 
 @Component({
   selector: 'app-user',
@@ -25,7 +26,9 @@ export class UserComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private userService: UserService,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router,
+    private toastService: ToastService,
   ) {
     this.userForm = this.fb.group({
       name: ['', [Validators.required]],
@@ -38,8 +41,24 @@ export class UserComponent implements OnInit {
   ngOnInit(): void {
     const user = this.authService.getDecodedToken();
 
+    if (!user || !user.sub) {
+      this.toastService.showToast({
+          title: 'Erro',
+          message: 'Erro ao obter os dados do usuário. Você não está logado.',
+          duration: 3000,
+          position: 'top-end'
+        }); 
+
+
+      setTimeout(() => {
+        this.authService.logout();
+        this.router.navigate(['/login']);
+      }, 1500);
+      return;
+    }
+
     this.userService.getCurrentUser(user.sub).subscribe({
-      next: (user: User) => {
+      next: (user: User) => {        
         this.userForm.setValue({
           name: user.name || '',
           lastName: user.last_name || '',
