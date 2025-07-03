cd036789e490db7347e11dd69ffded836583d654
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-07-02 21:10:18 -0300

    adding selecton of color scheme back

--- frontend/src/app/layout/default-layout/default-header/default-header.component.ts
+++ frontend/src/app/layout/default-layout/default-header/default-header.component.ts
@@ -31,13 +31,22 @@ import { IconDirective } from '@coreui/icons-angular';
   imports: [ContainerComponent, HeaderTogglerDirective, SidebarToggleDirective, IconDirective, HeaderNavComponent, NavLinkDirective, RouterLink, NgTemplateOutlet, BreadcrumbRouterComponent, DropdownComponent, DropdownToggleDirective, AvatarComponent, DropdownMenuDirective, DropdownHeaderDirective, DropdownItemDirective, BadgeComponent, DropdownDividerDirective]
 })
 export class DefaultHeaderComponent extends HeaderComponent {
-
+  
   readonly #colorModeService = inject(ColorModeService);
   readonly colorMode = this.#colorModeService.colorMode;
 
   readonly colorModes = [
-    { name: 'dark', text: 'Dark', icon: 'cilMoon' }
+    { name: 'light', text: 'Light', icon: 'cilSun' },
+    { name: 'dark', text: 'Dark', icon: 'cilMoon' },
+    { name: 'auto', text: 'Auto', icon: 'cilContrast' }
   ];
+
+  readonly icons = computed(() => {
+    const currentMode = this.colorMode();
+    return this.colorModes.find(mode => mode.name === currentMode)?.icon ?? 'cilSun';
+  });
+
+
 
   constructor(private authService: AuthService, private router: Router) {
     super();
