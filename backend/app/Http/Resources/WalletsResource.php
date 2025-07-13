5658a3d53860d5b5438774ce082519be16b84a98
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-07-13 18:44:31 -0300

    returning wallet id to make stocks requests

--- backend/app/Http/Resources/WalletsResource.php
+++ backend/app/Http/Resources/WalletsResource.php
@@ -9,6 +9,7 @@ class WalletsResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
             'balance' => $this->balance,
