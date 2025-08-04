415f6542f47a68a27361bd66f5156a040e9a01dc
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-08-04 20:48:51 -0300

    feat: add jobs table and create test job

---
+++ backend/app/Jobs/TestJob.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class TestJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function handle(): void
+    {
+        Log::info('TestJob executed with data:', ['data' => $this->data]);
+    }
+}
