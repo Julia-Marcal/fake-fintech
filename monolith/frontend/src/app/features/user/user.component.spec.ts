ac0eeddce7ce0281f208c7cdf65ecffc9a62e9d1
Julia-Marcal <anajulia.05marcal@gmail.com>
2025-10-07 20:40:07 -0300

    chore: update the project to fit the new stack I'm currently studing

--- monolith/frontend/src/app/features/user/user.component.spec.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { UserComponent } from './user.component';
-
-describe('UserComponent', () => {
-  let component: UserComponent;
-  let fixture: ComponentFixture<UserComponent>;
-
-});
